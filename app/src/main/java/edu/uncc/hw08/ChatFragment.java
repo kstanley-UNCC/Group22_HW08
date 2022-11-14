510f8eb438eae074e86e11c48ecc714ad289bfd7
Ken Stanley <dohpaz@gmail.com>
2022-11-13 21:27:14 -0500

    Wire up the delete chat button

--- app/src/main/java/edu/uncc/hw08/ChatFragment.java
+++ app/src/main/java/edu/uncc/hw08/ChatFragment.java
@@ -4,10 +4,12 @@
 
 package edu.uncc.hw08;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -16,6 +18,7 @@ import android.view.ViewGroup;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import edu.uncc.hw08.databinding.FragmentChatBinding;
 
@@ -25,6 +28,8 @@ import edu.uncc.hw08.databinding.FragmentChatBinding;
  */
 public class ChatFragment extends Fragment {
     Chat chat;
+
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     public ChatFragment(Chat chat) {
@@ -53,6 +58,27 @@ public class ChatFragment extends Fragment {
         assert firebaseUser != null;
 
         requireActivity().setTitle(getString(R.string.chat_label, firebaseUser.getUid().equals(chat.getOwner()) ? chat.getReceiverName() : chat.getOwnerName()));
+
+        binding.buttonDeleteChat.setOnClickListener(v -> {
+            firebaseFirestore
+                    .collection("Chats")
+                    .document(chat.getId())
+                    .delete()
+                    .addOnCompleteListener(task -> {
+                        if (!task.isSuccessful()) {
+                            Exception exception = task.getException();
+                            assert exception != null;
+                            new AlertDialog.Builder(requireContext())
+                                    .setTitle("An Error Occurred")
+                                    .setMessage(exception.getLocalizedMessage())
+                                    .setPositiveButton("Ok", (dialog, which) -> dialog.dismiss())
+                                    .show();
+                            return;
+                        }
+
+                        mListener.gotoMyChats();
+                    });
+        });
 
         binding.buttonClose.setOnClickListener(v -> mListener.gotoMyChats());
     }
